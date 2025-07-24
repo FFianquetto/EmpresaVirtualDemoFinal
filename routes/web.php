f3e2012305672636844fb152a7480a012515781d
Fernando <fernandocancino2004@gmail.com>
2025-07-23 21:58:03 -0600

    v1

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+use App\Http\Controllers\ProductController; 
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('products', ProductController::class); 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
